17d21d47eeead6540e103bd971e563024ed56245
ben256dev <benjamin@ben256.com>
2025-02-01 22:19:58 -0700

    generation script

--- convert.sh
+++ convert.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <markdown_file>"
+usage() {
+    echo "usage: $0 [-n|--no-preview] <markdown_file>"
     exit 1
+}
+
+no_preview=false
+
+while [[ "$1" =~ ^- ]]; do
+    case "$1" in
+        -n|--no-preview)
+            no_preview=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+if [[ -z "$1" ]]; then
+    usage
 fi
 
 file_name=$(basename $1 .md)
 pandoc "$file_name.md" -o "$file_name.html" --template=read.html --metadata last-updated="$(date +'%Y-%m-%d %H:%M:%S')"
-firefox "$file_name.html" 1>/dev/null 2>/dev/null &
+
+if ! $no_preview; then
+    firefox "$file_name.html" 1>/dev/null 2>/dev/null &
+fi
---
+++ generate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for f in *.md; do
+    echo -ne "  ...\r"
+    ./convert.sh -n "$f"
+    new_file="$(basename "$f" .md).html"
+
+    echo -ne "\033[2K  ...\r"
+    mv "$new_file" ../public/read/
+    echo -ne "\033[2K $new_file\r\n"
+done
